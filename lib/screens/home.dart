9a0383592d4ea8475b88b189877eb9d7f089926d
Enzo Nakashima <enzonakashima@outlook.com>
2023-01-21 16:44:19 -0300

    Search box

--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -12,7 +12,14 @@ class Home extends StatefulWidget {
 
 class _HomeState extends State<Home> {
   final todosList = ToDo.todoList();
+  List<ToDo> _foundToDo = [];
   final _todoController = TextEditingController();
+
+  @override
+  void initState() {
+    _foundToDo = todosList;
+    super.initState();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -38,7 +45,7 @@ class _HomeState extends State<Home> {
                         ),
                       ),
                     ),
-                    for (ToDo todoo in todosList)
+                    for (ToDo todoo in _foundToDo.reversed)
                       ToDoItem(
                         todo: todoo,
                         onToDoChanged: _handleToDoChange,
@@ -107,17 +114,37 @@ class _HomeState extends State<Home> {
   void _deleteToDoItem(String id) {
     setState(() {
       todosList.removeWhere((item) => item.id == id);
+      _foundToDo.removeWhere((item) => item.id == id);
     });
   }
 
   void _addToDoItem(String toDo) {
     setState(() {
-      todosList.add(ToDo(
-          id: DateTime.now().millisecondsSinceEpoch.toString(),
-          todoText: toDo));
+      if (toDo.isNotEmpty) {
+        todosList.add(ToDo(
+            id: DateTime.now().millisecondsSinceEpoch.toString(),
+            todoText: toDo));
+      }
     });
 
     _todoController.clear();
+  }
+
+  void _runFilter(String enteredKeyword) {
+    List<ToDo> results = [];
+    if (enteredKeyword.isEmpty) {
+      results = todosList;
+    } else {
+      results = todosList
+          .where(((item) => item.todoText!
+              .toLowerCase()
+              .contains(enteredKeyword.toLowerCase())))
+          .toList();
+    }
+
+    setState(() {
+      _foundToDo = results;
+    });
   }
 
   Widget searchBox() {
@@ -126,6 +153,7 @@ class _HomeState extends State<Home> {
       decoration: BoxDecoration(
           color: Colors.white, borderRadius: BorderRadius.circular(20)),
       child: TextField(
+        onChanged: (value) => _runFilter(value),
         decoration: InputDecoration(
           contentPadding: EdgeInsets.all(0),
           prefixIcon: Icon(
